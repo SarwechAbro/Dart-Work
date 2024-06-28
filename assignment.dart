8c3cb9df702194a4321286d3846bcee71c0f5476
SarwechAbro <sarwechabro@gamil.com>
2024-06-28 15:21:40 +0500

    new changes added

---
+++ assignment.dart
@@ -0,0 +1,9 @@
+void main() {
+  double Length = 10.0;
+  double Breadth = 5.0;
+
+  double area = Length * Breadth;
+
+  print(
+      'The area of the rectangle with length $Length and breadth $Breadth is: $area');
+}
---
+++ tempreture.dart
@@ -0,0 +1,7 @@
+void main() {
+  double celsius = 20.0;
+
+  double fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
+
+  print('$celsius degrees Celsius is equal to $fahrenheit degrees Fahrenheit');
+}
