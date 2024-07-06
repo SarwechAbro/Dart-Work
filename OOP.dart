7580808c198363df355a90278266fd8890a47961
SarwechAbro <SarwechAbro>
2024-07-06 19:27:42 +0500

    first commit

--- OOP.dart
+++ OOP.dart
@@ -1,1 +1,11 @@
-void main() {}
+void main() {
+  Car car1 = Car(name: 'Suzuki', yearModel: 1996);
+  print(car1.name);
+  print(car1.yearModel);
+}
+
+class Car {
+  String? name;
+  int? yearModel;
+  Car({this.name, required this.yearModel});
+}
